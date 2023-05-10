515346fcfd00a3428719deb664ced1a566726334
Clockedz <epixplayz222@gmail.com>
2023-05-09 23:19:26 -0700

    edited index

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Navbar() {
 	return (
-		<nav className="flex items-center justify-center fixed text-xl z-10 font-text bg-gradient-to-b from-black to-transparent">
+		<nav className="flex items-center justify-center fixed text-xl z-10 font-text">
 			Home
 		</nav>
 	);
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,26 +3,27 @@ import Layout from "@components/layout";
 import Image from "next/image";
 
 export default function Home() {
-	return (
-		<Layout>
-			<main>
-				<div className="relative h-screen w-auto overflow-x-hidden">
-					<video
-						src={require("@public/videos/video.mp4")}
-						autoPlay
-						loop
-						muted
-						className="w-full h-full object-cover z-0"
-					></video>
-					<div className="flex justify-center items-center absolute top-0 left-0 w-full h-full font-text flex-col">
-						<p className="text-5xl block">Japan's</p>
-						<p className="text-5xl block mt-4">
-							Prestigious Lineage
-						</p>
-					</div>
-				</div>
-				<div className="w-full">origin</div>
-			</main>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <main>
+        <div className="relative h-screen w-auto overflow-hidden">
+          <video
+            src={require("@public/videos/video.mp4")}
+            autoPlay
+            loop
+            muted
+            className="w-full h-full object-cover z-0"
+          ></video>
+          reverse
+          <div className="flex justify-center items-center absolute top-0 left-0 w-full h-full font-text flex-col text-5xl text-center sm:text-left">
+            <p className="block">Japan's</p>
+            <p className="block w-100 pt-2">Prestigious Lineage</p>
+          </div>
+        </div>
+        <section className="font-text mx-auto my-10vw">
+          <p className="font-text text-3xl absolute mt-12">Origin</p>
+        </section>
+      </main>
+    </Layout>
+  );
 }
