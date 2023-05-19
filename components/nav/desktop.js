5682dcaab5626f831063d94bcc9565e71c3c544c
Clockedz <epixplayz222@gmail.com>
2023-05-19 16:16:25 -0700

    updated navbar

--- components/nav/desktop.js
+++ components/nav/desktop.js
@@ -1,56 +1,95 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import {useRouter} from "next/router";
-import { triangle } from "./data";
-import { filledCircle } from "./data";
-import { circle } from "./data";
+import anime from "animejs";
+import {triangle} from "./data";
+import {filledCircle} from "./data";
+import {circle} from "./data";
 
 const Desktop = () => {
-  const router = useRouter();
-  const [home, setHome] = React.useState(true);
-  const [family, setFamily] = React.useState(false);
-  const [lookbook, setLookbook] = React.useState(false);
-  const [visibility, setVisibiity] = React.useState();
+	const router = useRouter();
+	const [home, setHome] = React.useState(true);
+	const [family, setFamily] = React.useState(false);
+	const [lookbook, setLookbook] = React.useState(false);
 
-  const isActive = (path) => {
-    return router.pathname === path ? "active" : "";
-  };
+	const circleRefs = useRef([]);
+	const triRefs = useRef([]);
 
-  if (isActive("/")) {
-    if (!home) {
-      setHome(true);
-      setFamily(false);
-      setLookbook(false);
-    }
-  }
+	const isActive = (path) => {
+		return router.pathname === path ? "active" : "";
+	};
 
-  if (isActive("/family")) {
-    if (!family) {
-      setHome(false);
-      setFamily(true);
-      setLookbook(false);
-    }
-  }
+	useEffect(() => {
+		const circles = circleRefs.current;
+		const triangles = triRefs.current;
 
-  if (isActive("/lookbook")) {
-    setHome(false);
-    setFamily(false);
-    setLookbook(true);
-  }
+		circles.forEach((circleElement, index) => {
+			anime({
+				targets: circleElement,
+				translateX: [-100, 0],
+				duration: 1000, // Animation duration in milliseconds
+				easing: "easeInOutCirc", // Easing function for the animation
+				delay: 100 * index,
+			});
+		});
+	}, []);
 
-  return (
-    <nav className="flex flex-wrap fixed text-xl bottom-0 z-10 font-text pl-4 mb-5 min-h-fit">
-      <div className="mt-1 pr-0.5 grid grid-cols-1 gap-1">
-        <div>{triangle}</div>
-        <div>{triangle}</div>
-        <div>{triangle}</div>
-      </div>
-      <div className="grid grid-cols-1 gap-1">
-        <div>{lookbook ? filledCircle : circle}</div>
-        <div>{family ? filledCircle : circle}</div>
-        <div>{home ? filledCircle : circle}</div>
-      </div>
-    </nav>
-  );
+	if (isActive("/")) {
+		if (!home) {
+			setHome(true);
+			setFamily(false);
+			setLookbook(false);
+		}
+	}
+
+	if (isActive("/family")) {
+		if (!family) {
+			setHome(false);
+			setFamily(true);
+			setLookbook(false);
+		}
+	}
+
+	if (isActive("/lookbook")) {
+		setHome(false);
+		setFamily(false);
+		setLookbook(true);
+	}
+
+	return (
+		<nav className="flex flex-wrap fixed text-xl bottom-0 z-10 font-text pl-4 mb-5">
+			<div className="pr-2 grid grid-cols-1 gap-2">
+				<div
+					className={lookbook ? "visible" : "invisible"}
+					ref={(eL) => (circleRefs.current[5] = eL)}
+				>
+					{triangle}
+				</div>
+				<div
+					className={family ? "visible" : "invisible"}
+					ref={(eL) => (circleRefs.current[4] = eL)}
+				>
+					{triangle}
+				</div>
+				<div
+					className={home ? "visible" : "invisible"}
+					ref={(eL) => (circleRefs.current[3] = eL)}
+				>
+					{triangle}
+				</div>
+			</div>
+			<div className="grid grid-cols-1 gap-2 mb-1">
+				<div ref={(el) => (circleRefs.current[2] = el)}>
+					{lookbook ? filledCircle : circle}
+				</div>
+				<div ref={(el) => (circleRefs.current[1] = el)}>
+					{family ? filledCircle : circle}
+				</div>
+				<div ref={(el) => (circleRefs.current[0] = el)}>
+					{home ? filledCircle : circle}
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Desktop;
